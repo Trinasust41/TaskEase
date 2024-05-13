8b3f77cad5de63d2c3be9176a4a1cf5c27f286e4
Trinasust41 <trinasustcse41@gmail.com>
2024-05-13 07:40:47 +0600

    first commit

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,88 @@
+import {
+	AppsOutline,
+	GridOutline,
+	HomeOutline,
+	LogOutOutline,
+	NewspaperOutline,
+	NotificationsOutline,
+	PeopleOutline,
+	PieChartOutline,
+} from "react-ionicons";
+
+const Sidebar = () => {
+	const navLinks = [
+		
+		{
+			title: "Boards",
+			icon: (
+				<AppsOutline
+					color="#555"
+					width="22px"
+					height="22px"
+				/>
+			),
+			active: true,
+		},
+		{
+			title: "Projects",
+			icon: (
+				<GridOutline
+					color="#555"
+					width="22px"
+					height="22px"
+				/>
+			),
+			active: false,
+		},
+		
+		{
+			title: "Workflows",
+			icon: (
+				<PeopleOutline
+					color="#555"
+					width="22px"
+					height="22px"
+				/>
+			),
+			active: false,
+		},
+		{
+			title: "Notifications",
+			icon: (
+				<NotificationsOutline
+					color="#555"
+					width="22px"
+					height="22px"
+				/>
+			),
+			active: false,
+		},
+		
+	];
+	return (
+		<div className="fixed left-0 top-0 md:w-[230px] w-[60px] overflow-hidden h-full flex flex-col">
+			<div className="w-full flex items-center md:justify-start justify-center md:pl-5 h-[70px] bg-[#fff]">
+				<span className="text-orange-400 font-semibold text-2xl md:block hidden">TaskEase.</span>
+				<span className="text-orange-400 font-semibold text-2xl md:hidden block">T.</span>
+			</div>
+			<div className="w-full h-[calc(100vh-70px)] border-r flex flex-col md:items-start items-center gap-2 border-slate-300 bg-[#fff] py-5 md:px-3 px-3 relative">
+				{navLinks.map((link) => {
+					return (
+						<div
+							key={link.title}
+							className={`flex items-center gap-2 w-full rounded-lg hover:bg-orange-300 px-2 py-3 cursor-pointer ${
+								link.active ? "bg-orange-300" : "bg-transparent"
+							}`}
+						>
+							{link.icon}
+							<span className="font-medium text-[15px] md:block hidden">{link.title}</span>
+						</div>
+					);
+				})}
+				
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
